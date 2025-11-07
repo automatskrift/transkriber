901c30902558e75adb0cf602864f597f1d6a9783
automatskrift <automatskrift@gmail.com>
2025-11-07 19:49:10 +0100

    Initial commit for SkrivDetNed macOS app
    Set up a new SwiftUI macOS project named SkrivDetNed. Includes project configuration files, asset catalogs, and basic ContentView and App entry point.

---
+++ SkrivDetNed/SkrivDetNed/SkrivDetNedApp.swift
@@ -0,0 +1,17 @@
+//
+//  SkrivDetNedApp.swift
+//  SkrivDetNed
+//
+//  Created by Tomas Thøfner on 07/11/2025.
+//
+
+import SwiftUI
+
+@main
+struct SkrivDetNedApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
